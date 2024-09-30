0dae0e829e4d9647e2fd64074880fc8ae713c23e
Escape <luckylilhore123@gmail.com>
2024-09-30 12:59:00 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -7,28 +7,7 @@
 // };
 
 function isValid(s) {
-     const stack = [];
-     const map = {
-         '(': ')',
-         '[': ']',
-         '{': '}'
-     };
- 
-     for (let i = 0; i < s.length; i++) {
-         let char = s[i];
-         
-         if (map[char]) {
-             // If it's an opening bracket, push to the stack
-             stack.push(char);
-         } else {
-             // If it's a closing bracket, check if it matches the last opened one
-             let last = stack.pop();
-             if (map[last] !== char) {
-                 return false;
-             }
-         }
-     }
-     return stack.length === 0;
+     
  }
  
  module.exports = { isValid };
