6879c70a3f5c6737b1f707fe9e26fe2d40f8fd68
Escape <luckylilhore123@gmail.com>
2024-09-30 13:16:00 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -5,28 +5,28 @@
 var isValid = function(s) {
      const stack = [];
      const map = {
-         '(': ')',
-         '[': ']',
-         '{': '}'
+     '(': ')',
+     '[': ']',
+     '{': '}'
      };
- 
+
      for (let i = 0; i < s.length; i++) {
-         let char = s[i];
-         
-         if (map[char]) {
-             stack.push(char);
-         } else {
-             let last = stack.pop();
-             if (map[last] !== char) {
-                 return false;
-             }
-         }
+     let char = s[i];
+     
+     if (map[char]) {
+          stack.push(char);
+     } else {
+          let last = stack.pop();
+          if (map[last] !== char) {
+               return false;
+          }
+     }
      }
      return stack.length === 0;
 };
- 
- module.exports = { isValid };
- 
+
+module.exports = { isValid };
+
 
 
 
